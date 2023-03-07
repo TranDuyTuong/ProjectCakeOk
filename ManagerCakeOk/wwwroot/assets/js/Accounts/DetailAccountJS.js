d7fdd1c06171c984650ad4f3f413ec0438eab613
TranDuyTuong <duytuong0503@gmail.com>
2023-03-07 21:51:56 +0700

    Update Foreach KnockoutJS

--- ManagerCakeOk/wwwroot/assets/js/Accounts/DetailAccountJS.js
+++ ManagerCakeOk/wwwroot/assets/js/Accounts/DetailAccountJS.js
@@ -1,42 +1,98 @@
 ﻿$(document).ready(function () {
-    ko.applyBindings(AccountStaff, $("html")[0]);
+    ko.applyBindings(staffdata, $("html")[0]);
     //get infomation account
-    CallApi()
+    /*    CallApi()*/
 });
-var Country = function (name, population) {
-    this.countryName = name;
-    this.countryPopulation = population;
-};
+function StudentModel(Count, FullName) {
+    var _Self = this;
+    _Self.Count = ko.observable(Count);
+    _Self.FullName = ko.observable(FullName);
+    _Self.FullNamesData = ko.observable(FullName.lastName + " " + FullName.fistName);
+}
 //Model View Model
-var AccountStaff = {
-    Email: ko.observable(),
-    CoustomerStaff: ko.observable(),
-    DateCreate: ko.observable(),
-    StaffChef: ko.observable(),
-    IdStaff: ko.observable(),
-    availableCountries: ko.observableArray([
-        new Country("VN", 2000),
-        new Country("US", 2000),
-        new Country("EN", 2000)
-    ]),
-    selectedCountry: ko.observable(),
+function staffdata() {
+    var _Self = this;
+    _Self.Email = ko.observable();
+    _Self.CoustomerStaff = ko.observable();
+    _Self.DateCreate = ko.observable();
+    _Self.StaffChef = ko.observable();
+    _Self.IdStaff = ko.observable();
+    _Self.FollowData = ko.observable();
+    _Self.Detail = [
+        { fistName: "Tuong", lastName: "Tran" },
+        { fistName: "Duong", lastName: "Nguyen" },
+        { fistName: "Nhu", lastName: "Huynh" },
+    ]
+    _Self.ListStudent = [];
 
-    InfoAccout: function () {
+    _Self.Detail.forEach(function (item, index) {
+        var DataItem = new StudentModel(index, item);
+        _Self.ListStudent.unshift(DataItem);
+    })
+
+    _Self.InfoAccout = function () {
         GetInfoStaff();
-    },
-    ChangeAccount: function () {
+    };
+    _Self.ChangeAccount = function () {
 
-    },
-    ChangeInfoAccout: function () {
+    };
+    _Self.ChangeInfoAccout = function () {
 
-    },
-    SaveAccount: function () {
+    };
+    _Self.SaveAccount = function () {
 
-    },
-    BlockAccount: function () {
+    };
+    _Self.BlockAccount = function () {
 
-    },
-};
+    }
+    //var Id = $("#IdAccount").val();
+    //if (Id.lenght == 0 || Id == '') {
+    //    toastr.error("Thông Báo Lỗi", "Id không chính xác hoặc bị bỏ trống!");
+    //} else {
+    //    $.ajax({
+    //        url: "/Account/DetailAccountPost",
+    //        type: "post",
+    //        data: {
+    //            IdAccount: Id
+    //        },
+    //        success: function (result) {
+    //            _Self.FollowData.subscribe(function () {
+    //                _Self.Email = result.email;
+    //                _Self.CoustomerStaff = result.nameCustomerOfStaff;
+    //                _Self.DateCreate = result.createDate;
+    //                _Self.StaffChef = result.nameStaffOrChef;
+    //                _Self.IdStaff = result.idStaff;
+    //                console.log(_Self.Email);
+    //            })
+    //        }
+    //    });
+    //    return;
+    //}
+
+}
+//var AccountStaff = function (){
+//    Email: ko.observable(),
+//    CoustomerStaff: ko.observable(),
+//    DateCreate: ko.observable(),
+//    StaffChef: ko.observable(),
+//    IdStaff: ko.observable(),
+
+//    InfoAccout: function () { 
+//        GetInfoStaff();
+//    },
+//    ChangeAccount: function () {
+
+//    },
+//    ChangeInfoAccout: function () {
+
+//    },
+//    SaveAccount: function () {
+
+//    },
+//    BlockAccount: function () {
+
+//    },
+//};
 //Call Api
 function CallApi() {
     var Id = $("#IdAccount").val();
@@ -50,11 +106,12 @@ function CallApi() {
                 IdAccount: Id
             },
             success: function (result) {
-                AccountStaff.Email(result.email);
-                AccountStaff.CoustomerStaff(result.nameCustomerOfStaff);
-                AccountStaff.DateCreate(result.createDate);
-                AccountStaff.StaffChef(result.nameStaffOrChef);
-                AccountStaff.IdStaff(result.idStaff);
+                var Vm = new staffdata();
+                Vm.Email = result.email;
+                Vm.CoustomerStaff(result.nameCustomerOfStaff);
+                Vm.DateCreate(result.createDate);
+                Vm.StaffChef(result.nameStaffOrChef);
+                Vm.IdStaff(result.idStaff);
             }
         });
         return;
